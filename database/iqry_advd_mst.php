f10121cfe0b6b3e867cc41080233dafd2d14df62
venkatesh <bogalavenkatesh333@gmailcom>
2023-09-14 17:36:43 +0530

    1

--- database/iqry_advd_mst.php
+++ database/iqry_advd_mst.php
@@ -32,7 +32,9 @@ if(isset($_POST['btnadvdsbmt']) && (trim($_POST['btnadvdsbmt']) != "") && isset(
 				$dadvdsource = $dadvdimgary[1];
 			}
 		}
-		  $iqryadvd_mst="INSERT into advd_mst(advdm_name, advdm_desc, advdm_prty, advdm_sts, advdm_dimgnm,advdm_lnk,advdm_crtdon, advdm_crtdby) values ('$name', '$desc','$prior', '$sts', '$dadvddest','$link','$curdt', '$ses_admin')"; 
+		$iqryadvd_mst="INSERT into advd_mst(advdm_name, advdm_desc, advdm_prty, advdm_sts, advdm_dimgnm,advdm_lnk,advdm_crtdon, advdm_crtdby) 
+		values
+		('$name', '$desc','$prior', '$sts', '$dadvddest','$link','$curdt', '$ses_admin')"; 
 		$irsadvd_mst= mysqli_query($conn,$iqryadvd_mst) or die(mysqli_error($conn));
 		if($irsadvd_mst==true)
 		{
